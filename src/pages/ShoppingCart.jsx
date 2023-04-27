630e582c002eeda33d9404a9e3939fbcf8d38789
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-04-27 16:27:15 -0300

    adicionei função de excluir produto do carrinho

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
+
 export default function ShoppingCart() {
-  
   const filteredProducts = JSON.parse(localStorage.getItem('cartItems')) || [];
+  const [products, setProducts] = useState(filteredProducts)
+
+  const removeProduct = (productID) => {
+    const newProducts = filteredProducts.filter(({ id }) => {
+      return id !== productID
+    });
+
+    setProducts(newProducts)
+    localStorage.setItem('cartItems', JSON.stringify(newProducts));
+  }
   return (
     <div>
-      { 
-        filteredProducts.map(({ id, name, thumbnail, price }) => (
+      {
+        products.map(({ id, name, thumbnail, price }) => (
           <div key={id}>
             <img src={thumbnail} alt="" />
             <p>{name}</p>
             <p>{price.toFixed(2)}</p>
-            <button>Excluir</button>
+            <button onClick={() => removeProduct(id)}>Excluir</button>
           </div>
         ))}
     </div>
