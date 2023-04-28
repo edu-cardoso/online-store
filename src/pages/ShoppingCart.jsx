83631cc16a64b3d66a21da61686cb51739073c93
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-04-28 12:27:58 -0300

    adicionei funcionalidade de controlar a quantidade de cada produto no carrinho

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -6,13 +6,14 @@ export default function ShoppingCart() {
   const filteredProducts = JSON.parse(localStorage.getItem('cartItems')) || [];
   const [products, setProducts] = useState(filteredProducts);
   const [totalPrice, settotalPrice] = useState(0);
+  const [quantity, setQuantity] = useState(0);
 
   useEffect(() => {
     const total = products.reduce((acc, curr) => {
-      return acc + curr.price
+      return acc + (curr.price * curr.quantity)
     }, 0)
     settotalPrice(total)
-  }, [])
+  }, [quantity, products])
 
   const removeProduct = (productID) => {
     const newProducts = filteredProducts.filter(({ id }) => {
@@ -34,18 +35,40 @@ export default function ShoppingCart() {
     settotalPrice(Number(updaTotalPrice.toFixed(2)))
   }
 
+  const incrementQuantity = (id) => {
+    const currProduct = products;
+    currProduct.find((item) => item.id === id).quantity += 1;
+    setQuantity(quantity + 1)
+    localStorage.setItem('cartItems', JSON.stringify(currProduct));
+  };
+
+  const decrementQuantity = (id) => {
+    const currProduct = products;
+    if (currProduct.find((item) => item.id === id).quantity > 1) {
+      currProduct.find((item) => item.id === id).quantity -= 1;
+      setQuantity(quantity - 1)
+    }
+    localStorage.setItem('cartItems', JSON.stringify(currProduct));
+  };
+
   return (
     <div>
       {products.length === 0 ? <p>Seu carrinho está vazio</p> :
-        products.map(({ id, name, thumbnail, price }) => (
+        products.map(({ id, name, thumbnail, price, quantity }) => (
           <div key={id}>
             <img src={thumbnail} alt="" />
             <p>{name}</p>
-            <p>{price.toFixed(2)}</p>
+            <p>{(price * quantity).toFixed(2)}</p>
             <button onClick={() => removeProduct(id)}>Excluir</button>
+            <div>
+              <button onClick={() => decrementQuantity(id)}>-</button>
+              <p>{quantity}</p>
+              <button onClick={() => incrementQuantity(id)}>+</button>
+            </div>
+
           </div>
         ))}
-        <p>{totalPrice.toFixed(2)}</p>
+      <p>{totalPrice.toFixed(2)}</p>
     </div>
   )
 }
