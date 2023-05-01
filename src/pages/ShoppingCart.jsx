932f2a0d532daa641b5cb63a9ac96a5043f1d2ed
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2023-05-01 16:26:31 -0300

    atualizei css do carrinho de compras

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -54,7 +54,7 @@ export default function ShoppingCart() {
   };
 
   return (
-    <div>
+    <div className={styles.shoppingCart}>
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       <header className={styles.headerContainer}>
         <Link to="/">
